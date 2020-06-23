9a1f22c041b9fd5a2bdbf94526f332bcc7541ba1
Leonardo Leite Meira dos Santos <leonardo.meira@dtidigital.com.br>
2020-06-23 14:31:11 -0300

    add build

---
+++ client/build/precache-manifest.2eec1f274848feeb5daa84ed731f61d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef2266c72d006f406ad079a165045b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78e26e48051473152f4a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "adb94562dca715961ac4",
+    "url": "/static/js/2.bca35990.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bca35990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e26e48051473152f4a",
+    "url": "/static/js/main.8d0cf7ff.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
